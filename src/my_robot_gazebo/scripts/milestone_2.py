0c7ab773c99515d0083bf2525679a18cd67e707a
PaulaSelim <s.paulakhalil@icloud.com>
2025-10-22 23:19:08 +0300

    Refactor milestone_2.py to correct joint command publishers and improve code structure

--- src/my_robot_gazebo/scripts/milestone_2.py
+++ src/my_robot_gazebo/scripts/milestone_2.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 import rospy 
 from geometry_msgs.msg import PoseStamped
-import numpy as np 
+import math
 from std_msgs.msg import Float64     
 
 rospy.init_node('milestone_2_controller', anonymous=False)  
 
 
-pub1 = rospy.Publisher('/Joint_3/command', Float64, queue_size=10)
-pub2 = rospy.Publisher('/milestone_2_pose', PoseStamped, queue_size=10)
 
+pub1 = rospy.Publisher('/Joint_1/command', Float64, queue_size=10)
+pub2 = rospy.Publisher('/Joint_2/command', Float64, queue_size=10)
+pub3 = rospy.Publisher('/Joint_3/command', Float64, queue_size=10)
+pub4 = rospy.Publisher('/Joint_4/command', Float64, queue_size=10)
+pub5 = rospy.Publisher('/Joint_5/command', Float64, queue_size=10)
+pub6 = rospy.Publisher('/milestone_2_pose', PoseStamped, queue_size=10)
+
+
+joint_1 = Float64()
+joint_2 = Float64()
 joint_3 = Float64()
+joint_4 = Float64()
+joint_5 = Float64()
+
 end_eff = PoseStamped()
 flag = 0
 x=0.0
@@ -43,9 +54,22 @@ sub1 = rospy.Subscriber('/end_effector_pose', PoseStamped, callback)
 while 1 and not rospy.is_shutdown():
    
     if flag == 1:
-        joint_3 = 0.5
-        pub1.publish(joint_3)
-        pub2.publish(end_eff)
+        
+        joint_1 = Float64(90 * (math.pi / 180))
+        joint_2 = Float64(90 * (math.pi / 180))
+        joint_3 = Float64(90 * (math.pi / 180))
+        joint_4 = Float64(0 * (math.pi / 180)) #this servo will not be used
+        joint_5 = Float64(90 * (math.pi / 180))
+
+      
+        pub1.publish(joint_1)
+        pub2.publish(joint_2)
+        pub3.publish(joint_3)
+        pub4.publish(joint_4)
+        pub5.publish(joint_5)
+
+        # Publishing the PoseStamped message to pub6
+        pub6.publish(end_eff)
         flag = 0
     rate.sleep()
 
